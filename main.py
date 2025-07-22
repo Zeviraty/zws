6f8b27625f0dbe11a6fb75ce3446301ab0c701c3
zeviraty <zeviraty@gmail.com>
2025-07-22 18:48:55 +0200

    Basic Body parsing and post functonality

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ MatchStrategy = Literal["exact", "startswith", "endswith", "contains"]
 class Route:
     def __init__(self, path: str, function: Callable, matching: MatchStrategy = "exact"):
         self.path = path
+        if not self.path.startswith("/"):
+            self.path = "/" + path
         self.function = function
         self.matching = matching
 
     def matches(self, input_path: str) -> bool:
+        '''Check if a path matches the defined route'''
         if self.matching == "exact":
             return input_path == self.path
         elif self.matching == "startswith":
@@ -26,6 +29,7 @@ class Route:
         return self.function(*args, **kwargs)
 
 def response(status:str = "200 OK", content:str = "", content_type:str = ""):
+    '''Build a response'''
     if content != "" and content_type != "":
         end = f"Content-Type: {content_type}\r\n\r\n{content}\n"
     elif content != "":
@@ -33,6 +37,12 @@ def response(status:str = "200 OK", content:str = "", content_type:str = ""):
     else:
         end = ""
     return f"HTTP/1.1 {status}\r\n{end}".encode()
+
+def rls(x: str):
+    '''Remove left spaces'''
+    for idx, i in enumerate(x):
+        if i != " ": break
+    return x[idx:]
 
 class Server():
     def __init__(self, fileshare = False):
@@ -55,11 +65,15 @@ class Server():
             buffer = connection.recv(1024)
             data = self.parse_request(buffer)
 
+            if data["method"] == "POST" and "body" not in data.keys():
+                buffer = connection.recv(1024)
+                data["body"] = self.parse_body(buffer, data["headers"]["content-type"])
+
             found = False
 
             for i in self.bound_paths:
                 if i.matches(data["path"]):
-                    i.call(connection)
+                    i.call(connection,data)
                     found = True
                     break
 
@@ -67,15 +81,15 @@ class Server():
                 try:
                     if os.path.exists("."+data["path"]):
                         content_type = "text/plain"
-                        match data["path"].endswith():
-                            case ".html": content_type = "text/html"
-                            case ".md": content_type = "text/markdown"
-                            case ".css": content_type = "text/css"
-                            case ".js": content_type = "text/javascript"
+                        match data["path"].split(".")[-1]:
+                            case "html": content_type = "text/html"
+                            case "md": content_type = "text/markdown"
+                            case "css": content_type = "text/css"
+                            case "js": content_type = "text/javascript"
                         connection.send(response(content_type = content_type, content = open("."+data["path"],'r').read()))
                         found = True
-                except:
-                    pass
+                except Exception as e:
+                    print(f"Error opening {data['path']}: {e}")
 
             if not found:
                 connection.send(response("404 NOT FOUND", content="404 not found"))
@@ -85,13 +99,48 @@ class Server():
     def parse_request(self,buffer: bytes):
         state = "status"
         headers = {}
-        for i in buffer.decode().split("\r\n"):
-            if i == "":
-                return {'method':method, 'path':path, 'version':version, 'headers':headers}
+        body = None
+        for idx, i in enumerate(buffer.decode().split("\r\n")):
             match state:
                 case "status":
                     method, path, version = i.split(" ")
                     state = "headers"
                 case "headers":
+                    if i == "":
+                        state = "body"
+                        continue
                     colon_split = i.split(":")
-                    headers[colon_split[0]] = ":".join(colon_split[1:])
+                    headers[rls(colon_split[0].lower())] = rls(":".join(colon_split[1:]))
+                case "body":
+                    if i == "":
+                        break
+                    body = self.parse_body(("\r\n".join(buffer.decode().split("\r\n")[idx:])).encode(),headers["content-type"])
+
+        data = {'method':method, 'path':path, 'version':version, 'headers':headers}
+        if body != None:
+            data["body"] = body
+        return data
+
+    def parse_body(self, buffer: bytes, content_type: str):
+        body = {}
+
+        match content_type:
+            case "application/x-www-form-urlencoded":
+                for i in buffer.decode().split("&"):
+                    key = i.split("=")[0]
+                    value = "=".join(i.split("=")[1:])
+                    body[key] = value
+
+        return body
+
+
+server = Server(True)
+server.start("127.0.0.1",8080)
+
+def index(connection, data):
+    if data["method"] == "POST":
+        connection.send(response(content_type="text/plain",content=f"Hello, {data['body']['name']}"))
+    else:
+        connection.send(response(content_type="text/html",content=open("index.html",'r').read()))
+
+server.bind_path("/", index)
